8cda637003946b44dbedb9881e9ea1242cc2507b
JaimePrada11 <jandrespradato.11@gmail.com>
2024-07-29 20:02:46 -0500

    feat: :sparkles: Agregar Funcion de Reportes
    Se creo una funcion de reportes para poder consultar todos los participantes y sus datos personales
    
    by @JaimePrada11

--- Registro_Datos.py
+++ Registro_Datos.py
@@ -4,7 +4,10 @@ from Validaciones import *
 
 def Registro():
     cargar_datos()
-
+    print("************************************************************")
+    print("                   REGISTRO ")
+    print("************************************************************\n")
+    
     usuario= {} 
     ID = input("Ingresa el ID: ")
     if not validar_cedula(ID):
@@ -17,4 +20,12 @@ def Registro():
         print("La cédula ya existe.")
     guardar_datos()
 
-Registro()
+def reporte():
+    cargar_datos()
+    print("************************************************************")
+    print("                   REPORTES")
+    print("************************************************************\n")
+
+    print(f"{'ID':<10} {'Nombre':<15} {'Apellido':<15} {'Correo':<25}")
+    for clave, valor in Informacion.items():
+        print(f"{clave:<10} {valor['Nombre']:<15} {valor['Apellido']:<15} {valor['Correo']:<25}")
