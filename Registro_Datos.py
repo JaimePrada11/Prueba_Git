d81529b3e4a76cba27578deabdd039f231bc219a
JaimePrada11 <jandrespradato.11@gmail.com>
2024-07-29 20:17:59 -0500

    feat: :sparkles: Agregar funcion de busqueda usuario
    Se agrego la funcion de buscar un usario especifico del json pidiendo el ID de este
    
    by @JaimePrada11

--- Registro_Datos.py
+++ Registro_Datos.py
@@ -26,6 +26,29 @@ def reporte():
     print("                   REPORTES")
     print("************************************************************\n")
 
-    print(f"{'ID':<10} {'Nombre':<15} {'Apellido':<15} {'Correo':<25}")
+    print(f"{'ID':<10} {'Nombre':<15} {'Apellido':<15} {'Correo':<20}")
+    print("-" * 60)
     for clave, valor in Informacion.items():
-        print(f"{clave:<10} {valor['Nombre']:<15} {valor['Apellido']:<15} {valor['Correo']:<25}")
+        print(f"{clave:<10} {valor['Nombre']:<15} {valor['Apellido']:<15} {valor['Correo']:<20}")
+    print("-" * 60)
+
+
+def buscar_id():
+    cargar_datos()
+    print("************************************************************")
+    print("                   REPORTE USUARIO ESPECIFICO")
+    print("************************************************************\n")
+
+    ID= input("Ingresa la cedula:  ")
+    if ID not in Informacion:
+        print("*" * 60)
+        print("El usuario con la cédula ingresada no existe. \nPor favor, intente de nuevo")
+        print("*" * 60)
+        return
+    else:
+        print(f"{'ID':<10} {'Nombre':<15} {'Apellido':<15} {'Correo':<20}")
+        print("-" * 60)
+        usuario = Informacion[ID]
+        print(f"{ID:<10} {usuario['Nombre']:<15} {usuario['Apellido']:<15} {usuario['Correo']:<20}")
+        print("-" * 60)
+
